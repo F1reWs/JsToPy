a7a55ebacf0f160913091f8649332ee70f085dd5
F1reW <97834402+F1reWs@users.noreply.github.com>
2023-05-08 21:05:16 +0300

    0.1.5

--- JsToPy/__init__.py
+++ JsToPy/__init__.py
@@ -1,8 +1,13 @@
 import builtins
 
-from . import *
+from .console import console
+from .require import require
 
 # Patch console
 builtins.console = console
 # Patch require
 builtins.require = require
+
+JsToPy = type('JsToPy', (), {'__getattr__': lambda _, name: getattr(builtins, name)})
+
+__main__ = JsToPy()
